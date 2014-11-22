cc5b7522ebe6342cf739e7661c69b92baf494d37
anonim1133 <anonim1133@wp.pl>
2014-11-22 18:24:57 +0100

    Dodane pliki odpowiedzialne za bazę danych

---
+++ app/src/main/java/me/anonim1133/ctf/DBConquered.java
@@ -0,0 +1,57 @@
+package me.anonim1133.ctf;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class DBConquered {
+	private static String TAG = "ZACJA_DB_CONQ";
+	private static String TABLE_NAME = "conquered";
+
+	private SQLiteDatabase db;
+
+	public DBConquered(SQLiteDatabase database){
+		this.db = database;
+	}
+
+	public boolean add(int points, String date,  double longitude, double latitude){
+		ContentValues values = new ContentValues();
+		values.put("points", points);
+		values.put("date", date);
+		values.put("longitude", longitude);
+		values.put("latitude", latitude);
+
+		try{
+			long value = db.insertOrThrow(TABLE_NAME, null, values);
+			Log.i(TAG, "Dodajno conquered z id: " + value);
+			if(value > 0) return true;
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+
+		return false;
+	}
+
+	public void getLast(int limit) {
+		Cursor cursor = db.query(TABLE_NAME , new String[] {"id", "points", "date",  "longitude", "latitude"}, null, null, null, "id DESC", "LIMIT " + limit);
+
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			int id = cursor.getInt(0);
+			String ssid = cursor.getString(1);
+			int signal = cursor.getInt(2);
+			int security = cursor.getInt(3);
+			double longitude = cursor.getDouble(4);
+			double latitude = cursor.getDouble(5);
+
+			//Wysłać to na serwer
+
+			//Skasować z lokalnej bazy danych
+			db.delete(TABLE_NAME, "id = ?", new String[] {"id"});
+
+			cursor.moveToNext();
+		}
+		cursor.close();
+	}
+}
---
+++ app/src/main/java/me/anonim1133/ctf/DataBaseHelper.java
@@ -0,0 +1,70 @@
+package me.anonim1133.ctf;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+	private static final String TAG = "DBHelper";
+	private static String DATABASE_NAME = "zacja.db";
+	private static final int DATABASE_VERSION = 1;
+
+	private Context c;
+	private SQLiteDatabase db;
+	private DBWifi wifi;
+
+	private static String DATABASE_CREATE = "BEGIN TRANSACTION;\n" +
+			"CREATE TABLE `wifi` (\n" +
+			"\t`id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+			"\t`ssid`\tTEXT,\n" +
+			"\t`signal`\tINTEGER DEFAULT '0',\n" +
+			"\t`security`\tINTEGER DEFAULT '0',\n" +
+			"\t`longitude`\tREAL DEFAULT '0',\n" +
+			"\t`latitude`\tREAL DEFAULT '0'\n" +
+			");\n" +
+			"CREATE TABLE `conquered` (\n" +
+			"\t`id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+			"\t`points`\tINTEGER DEFAULT '0',\n" +
+			"\t`date`\tINTEGER DEFAULT '0',\n" +
+			"\t`longitude`\tREAL DEFAULT '0',\n" +
+			"\t`latitude`\tREAL DEFAULT '0'\n" +
+			");\n" +
+			"CREATE INDEX `long_index` ON `conquered` (`longitude` ASC);\n" +
+			"CREATE INDEX `lat_index` ON `conquered` (`latitude` ASC);\n" +
+			"CREATE INDEX `date_index` ON `conquered` (`date` DESC);\n" +
+			"COMMIT;\n";
+
+	public DataBaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		this.c = context;
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+    db.execSQL("DROP TABLE IF EXISTS wifi; DROP TABLE IF EXISTS conquered;");
+    onCreate(db);
+	}
+
+	public void open() throws SQLException {
+		db = getWritableDatabase();
+
+	}
+
+	public boolean addWifi(String ssid, int signal, int security, double longitude, double latitude){
+		return wifi.add(ssid, signal, security, longitude, latitude);
+	}
+
+	public void sendWifi() {
+		wifi.send();
+	}
+}
+
+
